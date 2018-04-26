b3813d50d194ada15991274220b075dd5ca7adf5
itggot-jacob-karlin <jacob.karlin@itggot.se>
2018-04-26 09:12:24 +0200

    15 mars till 19 april

--- app.rb
+++ app.rb
@@ -1,13 +1,59 @@
+require_relative 'auth.rb'
+include Auth 
+require 'session'
+
 class App < Sinatra::Base
 
 	enable :sessions
 
 	get '/' do
-		slim(:index)
-	end
-
-	get '/login' do
 		slim(:login)
 	end
 
+	get '/homepage' do
+		slim(:homepage)
+	end
+
+	post '/login' do
+		username = params[:username]
+		password = params[:password]
+
+		id = login_user(username, password, open_database())
+		if id == -1 
+			return redirect('/login')
+		else
+			return redirect('/homepage')
+		end
+	end
+
+	get '/register' do
+		slim(:register)
+	end
+
+	post '/register' do
+		username = params[:username].strip
+		password = params[:password]
+		password_confirm = params[:password_confirm]
+		puts "Password: #{password}, confirm: #{password_confirm}"
+
+		if password != password_confirm 
+			puts "Passwords don't match!"
+			return redirect('/register')
+		end
+
+		db = open_database()
+		user = get_user_by_username(username, db)
+		if user != nil
+			puts "User with username '#{username}' already exists!"
+			return redirect('register')
+		end
+		register_user(username, password, db)
+
+		return redirect('/')
+	end
+
+	get '/logout' do
+		logout_user()
+		return redirect('/') 
+	end
 end           
---
+++ auth.rb
@@ -0,0 +1,58 @@
+require 'session' 
+
+module Auth
+
+    def open_database()
+        db = SQLite3::Database.new('db/db.sqlite3')
+        db.results_as_hash = true
+        return db
+    end
+
+    def get_user_by_username(username, db)
+        users = db.execute("SELECT * FROM users WHERE username = '#{username}'")
+        if users.length == 0 
+            return nil
+        end
+        return users[0] 
+    end
+
+    def register_user(username, password, db)
+        password_encrypted = BCrypt::Password.create(password)
+        result = db.execute("INSERT INTO users(username, password) VALUES(?, ?)", [username, password_encrypted])
+        id = db.execute("SELECT id FROM users WHERE username = '#{username}'")[0]["id"]
+        session[:user_id] = id
+    end
+
+    def login_user(username, password, db)
+        users = db.execute("SELECT * FROM users WHERE username = '#{username}'")
+        if users.length == 0 
+            return -1 
+        end
+    
+        users = users[0]
+        if BCrypt::Password.new(users['password']) == password
+            id = users['id']
+            session[:user_id] = id 
+            return id
+        end
+
+        return -1
+    end
+
+    def logout_user()
+        session[:user_id] = nil
+    end
+
+    def get_user_id()
+        id = session[:user_id]
+        if id == nil
+            return -1
+        end
+        return id
+    end 
+
+    def is_logged_in()
+        return get_user_id() != -1
+    end
+end
+
